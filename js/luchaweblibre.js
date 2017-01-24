e82680e2fed548404f92d42543053014e926b66c
Diego <diekus@gmail.com>
2017-01-24 10:58:50 +0000

    -dev

--- js/luchaweblibre.js
+++ js/luchaweblibre.js
@@ -10,22 +10,52 @@ TextureCache = PIXI.utils.TextureCache;
 /* GLOBAL VARIABLES */
 let stage = null,
  renderer = null,
- state = null,
- logo = null;
+ state = null;
+
+/* SPRITES */
+ let logo = null,
+ pWalk = null;
 
  /* FUNCTIONS */
-
 function init(){
     stage = new Container();
-    renderer = autoDetectRenderer(window.innerWidth, window.innerHeight,
-    {transparent:true});
+    renderer = autoDetectRenderer(window.innerWidth, window.innerHeight,{transparent:true});
     renderer.autoResize = true;
     document.body.appendChild(renderer.view);
 
     // window.addEventListener("resize", event => {  scaleToWindow(renderer.view); });
+    //sets keyboard event
+    let upKey = keyboard(38);
+    let downKey = keyboard(40);
+    let leftKey = keyboard(37);
+    let rightKey = keyboard(39);
+
+    upKey.press = () => {};
+    upKey.release = () => {};
+    downKey.press = () => {};
+    downKey.release = () => {};
+    leftKey.press = () => {};
+    leftKey.release = () => {};
+    rightKey.press = () => {
+        walk(pWalk, 'right', true);
+    };
+    rightKey.release = () => {
+        walk(pWalk, 'right', false);
+    };
 
         
     loadAssets();    
+}
+
+function walk(spr, dir, motion, speed){
+    if(motion){
+        spr.play();
+        spr.vx = speed;
+    }
+    else{
+        spr.stop();
+        spr.vx = 0;
+    }
 }
 
 function loadAssets(){
@@ -38,26 +68,23 @@ function loadAssets(){
 function setup(){
     //manage assets
     logo = new Sprite(resources['img/luchaWebLibre.png'].texture);
-    stage.addChild(logo);
     logo.position.set(150, 150);
     logo.anchor.set(.5, .5);
     logo.scale.set(.5, .5);
+    stage.addChild(logo);
 
     let su = new SpriteUtilities(PIXI, renderer);
     let pWalkTextures = [resources['img/panda.json'].textures['spWalk1.png'], resources['img/panda.json'].textures['spWalk2.png']];
-    let pWalk = new MovieClip(pWalkTextures);
+    pWalk = new MovieClip(pWalkTextures);
     pWalk.position.set(250, 250);
     pWalk.anchor.set(.5,.5);
-    pWalk.play();
     pWalk.animationSpeed = .1;
     stage.addChild(pWalk);
 
-    let phead = new Sprite(resources['img/panda.json'].textures['spHead.png']);
+    let phead = new Sprite(TextureCache['spHead.png']);
     phead.anchor.set(.5,.5);
     phead.position.set(250, 190);
     stage.addChild(phead);
-
-
 
     //set the game state
     state = play;
@@ -73,7 +100,8 @@ function gameLoop(){
 }
 
 function play(){
-    
+    pWalk.x += pWalk.vx;
+    logo.x += 1;
 }
 
 function end(){
